8b35cc57fbef1841e09145c1a112b04183570c0f
Joel Fogue <joel.fogue@quickpivot.com>
2020-02-22 00:53:06 -0500

    Updates to jenkinsfile

--- jenkins_home/workspace/k8sapp@tmp/durable-0fcc9da8/script.sh
+++
@@ -1,5 +0,0 @@
-
-                echo "Listing dir content"
-                ls -al 
-                scp -o StrictHostKeyChecking=no app/services.yml app/nginx-pod.yml ubuntu@18.232.88.23:/home/ubuntu/k8sapp
-                
