0eb5861710e01c28a904575a8c5fa8a6842faef4
Joel Fogue <joel.fogue@quickpivot.com>
2020-02-23 18:51:04 -0500

    Debugging

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+node_app=`docker ps -a | grep jenkins-container | awk '{print $NF}'`
+if [ $node_app=='jenkins-container' ]; then
+    echo "jenkins-container is running, lets delete"
+        docker rm -f jenkins-container
+fi
+
+# images=`docker images | grep dind/jenkins | awk '{print $3}'`
+# docker rmi $images
+
 sudo docker run --name jenkins-container -p 8081:8080 \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /home/joel/workspace/k8sapp/jenkins_home:/var/jenkins_home \
